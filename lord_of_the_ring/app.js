eb6b0ea63393c43cd7637fb276fb03582a85c4ba
Владимир <vladimir@rupor.agency>
2022-03-06 15:56:49 +0400

    learn CommonJs

---
+++ lord_of_the_ring/app.js
@@ -0,0 +1,20 @@
+const a = -1;
+
+if (a >= 0) {
+	const log = require('./charactres');
+	log();
+} else {
+	console.log('a < 0');
+}
+
+// let myChars = charactres;
+
+// myChars = stealRing(myChars, 'Фродо');
+
+// for(const c of charactres){
+// 	console.log(c);
+// }
+
+
+
+module.exports = { a };
---
+++ lord_of_the_ring/charactres.js
@@ -0,0 +1,19 @@
+console.log('download charatres,js')
+// let charactres = [
+// 	{name:'Фродо',hasRing: false},
+// 	{name: 'Бильбо', hasRing: false},
+// ]
+
+// function stealRing(charactres, owner){
+// 	return charactres.map(c => {
+// 		if(c.name == owner){
+// 			c.hasRing = true;
+// 		}else{
+// 			c.hasRing = false;
+// 		}
+// 	})
+// }
+module.exports = function log(){
+	console.log('log');
+}
+// module.exports = {charactres, stealRing};
